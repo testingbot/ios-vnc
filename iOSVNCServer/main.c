b7e297fb3f16b37b69d382d970a2adac75e30744
Michal Pietras <pietrasm@me.com>
2016-06-29 15:27:19 +0200

    Adds missing main.c

---
+++ iOSVNCServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  iOSVNCServer
+//
+//  Created by Michal Pietras on 28/06/2016.
+//  Copyright © 2016 TestingBot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
