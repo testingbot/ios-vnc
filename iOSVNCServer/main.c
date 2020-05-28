5f16e31dd597ddf4489e635a18a8c7ed03178f03
jochen <jochen@netlog.com>
2020-05-28 16:29:52 +0200

    merge changes upstream

--- iOSVNCServer/main.c
+++ iOSVNCServer/main.c
@@ -15,7 +15,7 @@
 #include <rfb/keysym.h>
 #include <png.h>
 #include <curl/curl.h>
-
+#include "./vendor/jsmn/jsmn.h"
 #include "iosscreenshot.h"
 
 #define DEFAULT_PORT 5901
@@ -42,6 +42,8 @@ typedef struct {
     char *keyURL;
     float scaleFactor;
     long int httpPort;
+    char *httpHost;
+    char *httpSessionID;
 } ScreenData;
 
 typedef struct {
@@ -50,6 +52,19 @@ typedef struct {
     DragRecognitionState dragRecognitionState;
     int dragStartX, dragStartY;
 } ClientData;
+
+struct DragDetails {
+    ScreenData *screen;
+    ClientData *client;
+    char *url;
+    int x;
+    int y;
+};
+
+struct MemoryStruct {
+    char *memory;
+    size_t size;
+};
 
 char keyboardChars[200] = "";
 
@@ -69,9 +84,12 @@ static void deinitClient(rfbClientPtr client);
 static void ptrHandler(int buttonMask, int x, int y, rfbClientPtr client);
 static void kbdHandler(rfbBool down, rfbKeySym key, rfbClientPtr client);
 static void *keyboardQueuer();
+static void *doDrag();
+static void *doTap();
 
 static int recognizeTap(int buttonMask, int x, int y, ClientData *clientData);
 static int recognizeDrag(int buttonMask, int x, int y, ClientData *clientData);
+static int validateSession(struct MemoryStruct chunk, ScreenData *screenData);
 
 static char *createURL(const char *host, const char *sessionID, const char *actionPath);
 
@@ -157,15 +175,15 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-    if (!(screenData->tapURL = createURL(HTTPHost, HTTPSessionID, "tap/0"))) {
+    if (!(screenData->tapURL = createURL(HTTPHost, HTTPSessionID, "private/tap"))) {
         fputs("ERROR: Cannot create a tap URL.\n", stderr);
         exit(-1);
     }
-    if (!(screenData->dragURL = createURL(HTTPHost, HTTPSessionID, "uiaTarget/0/dragfromtoforduration"))){
+    if (!(screenData->dragURL = createURL(HTTPHost, HTTPSessionID, "private/swipe"))){
         fputs("ERROR: Cannot create a drag URL.\n", stderr);
         exit(-1);
     }
-    if (!(screenData->keyURL = createURL(HTTPHost, HTTPSessionID, "keys"))) {
+    if (!(screenData->keyURL = createURL(HTTPHost, HTTPSessionID, "wda/keys"))) {
         fputs("ERROR: Cannot create a keys URL.\n", stderr);
         exit(-1);
     }
@@ -173,7 +191,9 @@ int main(int argc, char **argv) {
     rfbScreen->screenData = screenData;
     fprintf(stderr, "Current key %s", screenData->keyURL);
     screenData->httpPort = HTTPPort;
-    
+    screenData->httpHost = (char*)HTTPHost;
+    screenData->httpSessionID = (char*)HTTPSessionID;
+
     pthread_t tid;
     pthread_create(&tid, NULL, &keyboardQueuer, screenData);
     
@@ -323,55 +343,223 @@ static void ptrHandler(int buttonMask, int x, int y, rfbClientPtr client) {
     if (buttonMask > 1) {
         return;
     }
+
     ScreenData *screenData = client->screen->screenData;
     ClientData *clientData = client->clientData;
     if (x >= 0 && x < client->screen->width &&
         y >= 0 && y < client->screen->height) {
-        if (recognizeTap(buttonMask, x, y, clientData)) {
-            char json[11 + 4 + 4 + 1];
-            if (curl_easy_setopt(screenData->curlHandle, CURLOPT_URL, screenData->tapURL) != CURLE_OK) {
-                fputs("ERROR: Cannot set an URL.\n", stderr);
-                exit(-1);
-            }
-
-            if (sprintf(json, "{\"x\":%d,\"y\":%d}", SCALE(x), SCALE(y)) < 0) {
-                fputs("ERROR: Cannot create JSON.\n", stderr);
-                exit(-1);
-            }
-            if (curl_easy_setopt(screenData->curlHandle, CURLOPT_POSTFIELDS, json) != CURLE_OK) {
-                fputs("ERROR: Cannot set JSON.\n", stderr);
-                exit(-1);
-            }
-            if (curl_easy_perform(screenData->curlHandle) != CURLE_OK) {
-                fputs("ERROR: Cannot send a request.\n", stderr);
-                exit(-1);
-            }
-        }
         if (recognizeDrag(buttonMask, x, y, clientData)) {
-            char json[46 + 4 + 4 + 4 + 4 + 1];
-            if (curl_easy_setopt(screenData->curlHandle, CURLOPT_URL, screenData->dragURL) != CURLE_OK) {
-                fputs("ERROR: Cannot set an URL.\n", stderr);
-                exit(-1);
-            }
-
-            if (sprintf(json, "{\"fromX\":%d,\"fromY\":%d,\"toX\":%d,\"toY\":%d,\"duration\":0}",
-                        SCALE(clientData->dragStartX), SCALE(clientData->dragStartY),
-                        SCALE(x), SCALE(y)) < 0) {
-                fputs("ERROR: Cannot create JSON.\n", stderr);
-                exit(-1);
-            }
-            if (curl_easy_setopt(screenData->curlHandle, CURLOPT_POSTFIELDS, json) != CURLE_OK) {
-                fputs("ERROR: Cannot set JSON.\n", stderr);
-                exit(-1);
-            }
-            if (curl_easy_perform(screenData->curlHandle) != CURLE_OK) {
-                fputs("ERROR: Cannot send a request.\n", stderr);
-                exit(-1);
-            }
+            pthread_t tid;
+            struct DragDetails *args = (struct DragDetails *)malloc(sizeof(struct DragDetails));
+            args->screen = screenData;
+            args->url = screenData->dragURL;
+            args->client = clientData;
+            args->x = x;
+            args->y = y;
+            pthread_create(&tid, NULL, doDrag, (void *)args);
+        } else if (recognizeTap(buttonMask, x, y, clientData)) {
+            pthread_t tid;
+            struct DragDetails *args = (struct DragDetails *)malloc(sizeof(struct DragDetails));
+            args->screen = screenData;
+            args->url = screenData->tapURL;
+            args->x = x;
+            args->y = y;
+            pthread_create(&tid, NULL, doTap, (void *)args);
         }
-
         clientData->lastX = x;
         clientData->lastY = y;
+    }
+}
+
+static size_t curlCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+ 
+  char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+  if (ptr == NULL) {
+    /* out of memory! */ 
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+ 
+  mem->memory = ptr;
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
+
+static void *doDrag(void *args) {
+    struct DragDetails *data = args;
+    char *url = data->url;
+    ScreenData *screenData = data->screen;
+    ClientData *clientData = data->client;
+    int x = data->x;
+    int y = data->y;
+    CURL *curlHandle;
+    curlHandle = curl_easy_init();
+    CURLcode res;
+    char errbuf[CURL_ERROR_SIZE];
+
+    struct MemoryStruct chunk;
+ 
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+    chunk.size = 0;    /* no data at this point */ 
+
+    char json[46 + 4 + 4 + 4 + 4 + 1];
+    if (curl_easy_setopt(curlHandle, CURLOPT_URL, url) != CURLE_OK) {
+        fputs("ERROR: Cannot set an URL.\n", stderr);
+    }
+    curl_easy_setopt(curlHandle, CURLOPT_ERRORBUFFER, errbuf);
+    if (curl_easy_setopt(curlHandle, CURLOPT_PORT, screenData->httpPort) != CURLE_OK) {
+        fputs("ERROR: Cannot set a port.\n", stderr);
+    }
+
+    if (sprintf(json, "{\"x\":%d,\"y\":%d,\"x1\":%d,\"y1\":%d}",
+                SCALE(clientData->dragStartX), SCALE(clientData->dragStartY),
+                SCALE(x), SCALE(y)) < 0) {
+        fputs("ERROR: Cannot create JSON.\n", stderr);
+    }
+    fprintf(stderr, "* Do Drag... %s\n", json);
+    if (curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, json) != CURLE_OK) {
+        fputs("ERROR: Cannot set JSON.\n", stderr);
+    }
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, curlCallback);
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void *)&chunk);
+    res = curl_easy_perform(curlHandle);
+    if (res != CURLE_OK) {
+        size_t len = strlen(errbuf);
+        fprintf(stderr, "\nlibcurl: (%d) ", res);
+        if (len) {
+            fprintf(stderr, "%s%s", errbuf, ((errbuf[len - 1] != '\n') ? "\n" : ""));
+        }
+        else {
+            fprintf(stderr, "%s\n", curl_easy_strerror(res));
+        }
+    }
+
+    curl_easy_cleanup(curlHandle);
+    if (validateSession(chunk, screenData) == 1) {
+        struct DragDetails *newArgs = (struct DragDetails *)malloc(sizeof(struct DragDetails));
+        newArgs->screen = screenData;
+        newArgs->url = screenData->dragURL;
+        newArgs->client = clientData;
+        newArgs->x = x;
+        newArgs->y = y;
+        doDrag(newArgs);
+    }
+}
+
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+    if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
+            strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+        return 0;
+    }
+    return -1;
+}
+
+static int validateSession(struct MemoryStruct chunk, ScreenData *screenData) {
+    jsmn_parser p;
+    jsmntok_t t[256]; /* We expect no more than 128 tokens */
+    int r;
+    int i;
+    char sessionId[65];
+    int changed = 0;
+    jsmn_init(&p);
+    r = jsmn_parse(&p, chunk.memory, strlen(chunk.memory), t, sizeof(t)/sizeof(t[0]));
+    if (r < 0) {
+        printf("Failed to parse JSON: %d\n", r);
+        return 0;
+    }
+
+    /* Assume the top-level element is an object */
+    if (r < 1 || t[0].type != JSMN_OBJECT) {
+        printf("Object expected\n");
+        return 0;
+    }
+
+    for (i = 1; i < r; i++) {
+        if (jsoneq(chunk.memory, &t[i], "sessionId") == 0) {
+            unsigned int length = t[i+1].end - t[i+1].start;
+            memcpy(sessionId, &chunk.memory[t[i+1].start], length);
+            sessionId[length] = '\0';
+        }
+    }
+    fprintf(stdout, "Curl data: %s\n", chunk.memory);
+
+    if (strcmp(screenData->dragURL, createURL(screenData->httpHost, sessionId, "private/swipe")) != 0) {
+        // fprintf(stdout, "SessionId has changed from %s to %s\n", screenData->httpSessionID, sessionId);
+        // reconstruct the URLs
+        // screenData->httpSessionID = sid;
+        screenData->dragURL = createURL(screenData->httpHost, sessionId, "private/swipe");
+        screenData->tapURL = createURL(screenData->httpHost, sessionId, "private/tap");
+        screenData->keyURL = createURL(screenData->httpHost, sessionId, "wda/keys");
+        changed = 1;
+    }
+    free(chunk.memory);
+
+    return changed;
+}
+
+static void *doTap(void *args)
+{
+    struct DragDetails *data = args;
+    ScreenData *screenData = data->screen;
+    char *url = data->url;
+    int x = data->x;
+    int y = data->y;
+    CURL *curlHandle;
+    char json[11 + 4 + 4 + 1];
+    curlHandle = curl_easy_init();
+    CURLcode res;
+    char errbuf[CURL_ERROR_SIZE];
+
+    struct MemoryStruct chunk;
+ 
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+    chunk.size = 0;    /* no data at this point */ 
+
+    if (curl_easy_setopt(curlHandle, CURLOPT_URL, url) != CURLE_OK) {
+        fputs("ERROR: Cannot set an URL.\n", stderr);
+    }
+    curl_easy_setopt(curlHandle, CURLOPT_ERRORBUFFER, errbuf);
+    if (curl_easy_setopt(curlHandle, CURLOPT_PORT, screenData->httpPort) != CURLE_OK) {
+        fputs("ERROR: Cannot set a port.\n", stderr);
+    }
+
+    fprintf(stdout, "{\"x\":%d,\"y\":%d}", SCALE(x), SCALE(y));
+
+    if (sprintf(json, "{\"x\":%d,\"y\":%d}", SCALE(x), SCALE(y)) < 0) {
+        fputs("ERROR: Cannot create JSON.\n", stderr);
+    }
+    if (curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, json) != CURLE_OK) {
+        fputs("ERROR: Cannot set JSON.\n", stderr);
+    }
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, curlCallback);
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+    fprintf(stderr, "* Do Tap... %s\n", json);
+    res = curl_easy_perform(curlHandle);
+    if (res != CURLE_OK) {
+        size_t len = strlen(errbuf);
+        fprintf(stderr, "\nlibcurl: (%d) ", res);
+        if (len) {
+            fprintf(stderr, "%s%s", errbuf, ((errbuf[len - 1] != '\n') ? "\n" : ""));
+        }
+        else {
+            fprintf(stderr, "%s\n", curl_easy_strerror(res));
+        }
+    }
+    curl_easy_cleanup(curlHandle);
+    if (validateSession(chunk, screenData) == 1) {
+        struct DragDetails *newArgs = (struct DragDetails *)malloc(sizeof(struct DragDetails));
+        newArgs->screen = screenData;
+        newArgs->url = screenData->tapURL;
+        newArgs->x = x;
+        newArgs->y = y;
+        doTap(newArgs);
     }
 }
 
